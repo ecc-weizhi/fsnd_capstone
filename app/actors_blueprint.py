e48806ccb947ecff8aa0fa189fce824250830e5a
ecc-weizhi <ecc.weizhi@gmail.com>
2021-08-13 22:52:20 -0700

    Implement GET /actors

--- app/actors_blueprint.py
+++ app/actors_blueprint.py
@@ -1,11 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from models.actor import Actor
 
 actors_blueprint = Blueprint("actors", __name__, url_prefix="/actors")
-
+ACTOR_PAGE_SIZE = 10
 
 @actors_blueprint.route("")
 def get():
-    pass
+    page = request.args.get('page', 1, type=int)
+
+    start_index_inclusive = (page - 1) * ACTOR_PAGE_SIZE
+    end_index_exclusive = start_index_inclusive + ACTOR_PAGE_SIZE
+
+    actors = Actor.query.all()
+    formatted_actors = [actor.format() for actor in actors]
+
+    return jsonify({
+        "success": True,
+        "actors": formatted_actors[start_index_inclusive:end_index_exclusive],
+    })
 
 
 @actors_blueprint.route("/<int:actor_id>", methods=["DELETE"])
