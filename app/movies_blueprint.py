b9b615e5bb7a8cc0a32925eea561abae5dc71927
ecc-weizhi <ecc.weizhi@gmail.com>
2021-08-13 22:52:28 -0700

    Implement GET /movies

--- app/movies_blueprint.py
+++ app/movies_blueprint.py
@@ -1,11 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from models.movie import Movie
 
 movies_blueprint = Blueprint("movies", __name__, url_prefix="/movies")
+MOVIE_PAGE_SIZE = 10
 
 
 @movies_blueprint.route("")
 def get():
-    pass
+    page = request.args.get('page', 1, type=int)
+
+    start_index_inclusive = (page - 1) * MOVIE_PAGE_SIZE
+    end_index_exclusive = start_index_inclusive + MOVIE_PAGE_SIZE
+
+    movies = Movie.query.all()
+    formatted_movies = [movie.format() for movie in movies]
+
+    return jsonify({
+        "success": True,
+        "actors": formatted_movies[start_index_inclusive:end_index_exclusive],
+    })
 
 
 @movies_blueprint.route("/<int:movie_id>", methods=['DELETE'])
