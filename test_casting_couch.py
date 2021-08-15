1e0ebadb11eb97c1f5b37c993290eb6608140ac8
ecc-weizhi <ecc.weizhi@gmail.com>
2021-08-15 14:30:09 -0700

    Fix token issue

--- test_casting_couch.py
+++ test_casting_couch.py
@@ -1,6 +1,8 @@
 import json
 import unittest
 from datetime import datetime
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 from app import create_app
 from models import setup_db, db
@@ -8,23 +10,64 @@ from models.actor import Actor
 from models.movie import Movie
 
 TEST_DB_PATH = "postgresql://weizhi:test1234@localhost:5432/castingcouch_test"
-
-CASTING_ASSISTANT_HEADERS = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY3VHhXcTZCVDA1RV9hZzdFTC1NWiJ9.eyJpc3MiOiJodHRwczovL2VjY3dlaXpoaS1mc25kLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTE4NThiMzhiNzJmNzAwNmFlNmNhZmMiLCJhdWQiOiJteUZvb2JhciIsImlhdCI6MTYyOTAxMjI5OCwiZXhwIjoxNjI5MDE5NDk4LCJhenAiOiJQdGdLN1FoYnVkdGYxWlVXbklOSGxiV3BhS0NwZ3JxVCIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiXX0.sBd9f4qT9BjI93hUt-yUVRI9cWQ3UgR_HRpJAhXKih_JUHg4w42i7T-mCZM6XRb1yH3duTWwaatAFQOeRTofQ8SmkIlnnOEZHJUCAXg-gnHHq_zV4AX8q8hkhH7ZinIQkaQ5xuXxbXweODdcpS7n0y8DysbJpaf7K524OzOb0Gt4wPSK5qk0kmfC-vfUJWKdVM4zoml4Sj0dAKoHEH_h6L4Q2VBK2RvyspNVAtHk9x51ySRYhRFFzEJOxUuDmgDjrqSmYWKBqPsCubi8EYTcXi2iK7nafWpvel9CJWIqCYT0zXWgtlilLE8brk486-7SwHfCeBnr9XABu07hLH1iTg"
-}
-CASTING_DIRECTOR_HEADERS = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY3VHhXcTZCVDA1RV9hZzdFTC1NWiJ9.eyJpc3MiOiJodHRwczovL2VjY3dlaXpoaS1mc25kLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTE4NTkxZTgxZDk5YjAwNzBlZjZlMjciLCJhdWQiOiJteUZvb2JhciIsImlhdCI6MTYyOTAxMjIwMSwiZXhwIjoxNjI5MDE5NDAxLCJhenAiOiJQdGdLN1FoYnVkdGYxWlVXbklOSGxiV3BhS0NwZ3JxVCIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImdldDphY3RvcnMiLCJnZXQ6bW92aWVzIiwicGF0Y2g6YWN0b3JzIiwicGF0Y2g6bW92aWVzIiwicG9zdDphY3RvcnMiXX0.HLyrmGxAFsS5pqf_t1KmLRMF7WBPC6Qlp2zJZn_5s0gI2RFa_pFUMDJFwg0mGTFqA3UbIrivp_S_rrG68eylQPyAaoZRHewKpkIYNbvEKcDp5StLmF4AZVz9O3OqaZPSy0UDJTkHaOPnHhDh4AQ0CTQ-ju20AF9TdvkU1TqQ8L2_c80hkOqyzQho1Knc0jX0j_hc4AoDX7qY5JtaLI7gPS-aAl_LXxjTfkRMmQnFq0uqp328D_FKwPQmp5GUjRSwf_ssL2UgxVXGN9uReFDtkRXOlghAjLRX_-pzzOHiaLpdMH6BUFU-60d7rtILazoUyCLfgD94efk7cUbgOxqMkg"
-}
-EXECUTIVE_PRODUCER_HEADERS = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY3VHhXcTZCVDA1RV9hZzdFTC1NWiJ9.eyJpc3MiOiJodHRwczovL2VjY3dlaXpoaS1mc25kLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTE4NTkzZGYwZDEzMTAwNzFmZjQ2NjYiLCJhdWQiOiJteUZvb2JhciIsImlhdCI6MTYyOTAxMTk0NSwiZXhwIjoxNjI5MDE5MTQ1LCJhenAiOiJQdGdLN1FoYnVkdGYxWlVXbklOSGxiV3BhS0NwZ3JxVCIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwiZ2V0Om1vdmllcyIsInBhdGNoOmFjdG9ycyIsInBhdGNoOm1vdmllcyIsInBvc3Q6YWN0b3JzIiwicG9zdDptb3ZpZXMiXX0.Ne02peqOaKowxYg7n82OL4PdxCy6pxXM5unt62fplvIfg7DIdE233CxhFlBLdBqtXEd_L4yswiiJ2n0ilCY2ACxsZ8ST_g8EoTt6FqEXw-5WhjHKQU2IGGxzLWtgU2UW2nOtJW-Skufgdj7kvxZGBxxfL5DvpqCJhlGBITQO3d02tlN4O1KikZ5Q3zCq-J0OlR-I59FaZhCx1J0su-bfBY_Vq1zJagNR3p7_GtDMihGZxJEPPbqIu9J-U5QtXR5esM6eWm8YbhEkX3NRn7KSyPr4eQwFWb0XoQblvFEi9LFQCbYTgiFZ0TkZ68sKWgHFQgEgJ4r3bkwg7yK-pJv0Tw"
-}
+CASTING_ASSISTANT_USERNAME = ""
+CASTING_ASSISTANT_PASSWORD = ""
+CASTING_DIRECTOR_USERNAME = ""
+CASTING_DIRECTOR_PASSWORD = ""
+EXECUTIVE_PRODUCER_USERNAME = ""
+EXECUTIVE_PRODUCER_PASSWORD = ""
+CLIENT_ID = ""
+CLIENT_SECRET = ""
 
 
 class CastingCouchTestCase(unittest.TestCase):
-    database_path = "postgresql://weizhi:test1234@localhost:5432/castingcouch_test"
+    _database_path = "postgresql://weizhi:test1234@localhost:5432/castingcouch_test"
+    _casting_assistant_headers = None
+    _casting_director_headers = None
+    _executive_producer_headers = None
+
+    @classmethod
+    def setUpClass(cls):
+        url = 'https://eccweizhi-fsnd.us.auth0.com/oauth/token'
+        post_fields = {
+            "grant_type": "password",
+            "audience": "myFoobar",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        }
+
+        # casting assistant
+        post_fields["username"] = CASTING_ASSISTANT_USERNAME
+        post_fields["password"] = CASTING_ASSISTANT_PASSWORD
+        request = Request(url, urlencode(post_fields).encode())
+        response = urlopen(request).read()
+        data = json.loads(response)
+        cls._casting_assistant_headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {data['access_token']}"
+        }
+
+        # casting director
+        post_fields["username"] = CASTING_DIRECTOR_USERNAME
+        post_fields["password"] = CASTING_DIRECTOR_PASSWORD
+        request = Request(url, urlencode(post_fields).encode())
+        response = urlopen(request).read()
+        data = json.loads(response)
+        cls._casting_director_headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {data['access_token']}"
+        }
+
+        # executive producer
+        post_fields["username"] = EXECUTIVE_PRODUCER_USERNAME
+        post_fields["password"] = EXECUTIVE_PRODUCER_PASSWORD
+        request = Request(url, urlencode(post_fields).encode())
+        response = urlopen(request).read()
+        data = json.loads(response)
+        cls._executive_producer_headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {data['access_token']}"
+        }
 
     def setUp(self):
         """Define test variables and initialize app."""
@@ -66,7 +109,7 @@ class CastingCouchTestCase(unittest.TestCase):
             db.drop_all()
 
     def test_get_actors_success(self):
-        res = self.client().get('/actors', headers=CASTING_ASSISTANT_HEADERS)
+        res = self.client().get('/actors', headers=self._casting_assistant_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -82,7 +125,7 @@ class CastingCouchTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
 
     def test_delete_actor_success(self):
-        res = self.client().delete('/actors/1', headers=CASTING_DIRECTOR_HEADERS)
+        res = self.client().delete('/actors/1', headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -90,7 +133,7 @@ class CastingCouchTestCase(unittest.TestCase):
         self.assertEqual(data['deleted_id'], 1)
 
     def test_id_not_exist_delete_actor_fail(self):
-        res = self.client().delete('/actors/10', headers=CASTING_DIRECTOR_HEADERS)
+        res = self.client().delete('/actors/10', headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -101,7 +144,7 @@ class CastingCouchTestCase(unittest.TestCase):
             "name": "Charlie",
             "age": 45,
             "gender": "F"
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -112,7 +155,7 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().post('/actors', json={
             "age": 45,
             "gender": "F"
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -123,7 +166,7 @@ class CastingCouchTestCase(unittest.TestCase):
             "name": "Ahmad",
             "age": 50,
             "gender": "F"
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -135,14 +178,14 @@ class CastingCouchTestCase(unittest.TestCase):
             "name": "Ahmad",
             "age": 50,
             "gender": "F"
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_get_movies_success(self):
-        res = self.client().get('/movies', headers=CASTING_ASSISTANT_HEADERS)
+        res = self.client().get('/movies', headers=self._casting_assistant_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -158,7 +201,7 @@ class CastingCouchTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
 
     def test_delete_movie_success(self):
-        res = self.client().delete('/movies/1', headers=EXECUTIVE_PRODUCER_HEADERS)
+        res = self.client().delete('/movies/1', headers=self._executive_producer_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -166,7 +209,7 @@ class CastingCouchTestCase(unittest.TestCase):
         self.assertEqual(data['deleted_id'], 1)
 
     def test_id_not_exist_delete_movie_fail(self):
-        res = self.client().delete('/movies/10', headers=EXECUTIVE_PRODUCER_HEADERS)
+        res = self.client().delete('/movies/10', headers=self._executive_producer_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -176,7 +219,7 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().post('/movies', json={
             "title": "Inception",
             "release_date": datetime.utcnow(),
-        }, headers=EXECUTIVE_PRODUCER_HEADERS)
+        }, headers=self._executive_producer_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -186,7 +229,7 @@ class CastingCouchTestCase(unittest.TestCase):
     def test_missing_field_add_movie_fail(self):
         res = self.client().post('/movies', json={
             "release_date": datetime.utcnow(),
-        }, headers=EXECUTIVE_PRODUCER_HEADERS)
+        }, headers=self._executive_producer_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -196,7 +239,7 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().patch('/movies/1', json={
             "title": "Harry Potter",
             "release_date": datetime.utcnow(),
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -207,14 +250,14 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().patch('/movies/10', json={
             "title": "Inception",
             "release_date": datetime.utcnow(),
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_casting_assistant_can_get_movies(self):
-        res = self.client().get('/movies', headers=CASTING_ASSISTANT_HEADERS)
+        res = self.client().get('/movies', headers=self._casting_assistant_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -226,7 +269,7 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().post('/movies', json={
             "title": "Inception",
             "release_date": datetime.utcnow(),
-        }, headers=CASTING_ASSISTANT_HEADERS)
+        }, headers=self._casting_assistant_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 403)
@@ -237,7 +280,7 @@ class CastingCouchTestCase(unittest.TestCase):
             "name": "Charlie",
             "age": 45,
             "gender": "F"
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -248,7 +291,7 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().post('/movies', json={
             "title": "Inception",
             "release_date": datetime.utcnow(),
-        }, headers=CASTING_DIRECTOR_HEADERS)
+        }, headers=self._casting_director_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 403)
@@ -258,7 +301,7 @@ class CastingCouchTestCase(unittest.TestCase):
         res = self.client().post('/movies', json={
             "title": "Inception",
             "release_date": datetime.utcnow(),
-        }, headers=EXECUTIVE_PRODUCER_HEADERS)
+        }, headers=self._executive_producer_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -266,7 +309,7 @@ class CastingCouchTestCase(unittest.TestCase):
         self.assertTrue(data['movie'])
 
     def test_executive_producer_can_delete_movie(self):
-        res = self.client().delete('/movies/1', headers=EXECUTIVE_PRODUCER_HEADERS)
+        res = self.client().delete('/movies/1', headers=self._executive_producer_headers)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
