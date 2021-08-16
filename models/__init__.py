e70239825f0759c1588a393ca1f0719b52a9885a
ecc-weizhi <ecc.weizhi@gmail.com>
2021-08-16 00:14:12 -0700

    Add hack to make database_uri from Heroku work

--- models/__init__.py
+++ models/__init__.py
@@ -8,8 +8,11 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path=local_database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", None) \
+    database_uri = os.environ.get("DATABASE_URL", None) \
         if os.environ.get("IS_HEROKU", None) else local_database_path
+    if database_uri.startswith("postgres://"):
+        database_uri = database_uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
