4ae3c018aec6317c6d893471ecb3e700a6e2a521
ecc-weizhi <ecc.weizhi@gmail.com>
2021-08-15 23:20:04 -0700

    Change to use db url from environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,15 @@
+import os
+
 from flask_sqlalchemy import SQLAlchemy
 
-database_path = "postgresql://weizhi:test1234@localhost:5432/castingcouch"
+local_database_path = "postgresql://weizhi:test1234@localhost:5432/castingcouch"
 
 db = SQLAlchemy()
 
 
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def setup_db(app, database_path=local_database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", None) \
+        if os.environ.get("IS_HEROKU", None) else local_database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
